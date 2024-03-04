b7ee95d7f8f13618fdc3dc6ffb5eae8960e7d905
Dihrey Araujo <dihreyh@gmail.com>
2024-03-04 20:02:01 -0300

    remove import pg

--- src/api/server.js
+++ src/api/server.js
@@ -1,4 +1,3 @@
-const { Pool } = require('pg');
 const app = require('./app.js');
 const phone = require('./config.js');
 require('dotenv').config();
@@ -9,12 +8,6 @@ const port = process.env.API_PORT || 3001;
 
 const Phone = PhoneModel(phone);
 const User = UserModel(phone);
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
-})
-
-pool.connect()
 
 app.listen(port, async () => {
   try {
