2b49ae016cce403261649c3a417c9310080d803a
Dihrey Araujo <dihreyh@gmail.com>
2024-03-04 20:59:32 -0300

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -11,14 +11,14 @@ const host = process.env.POSTGRESQL_HOST;
 
 const port = process.env.POSTGRESQL_PORT;
 
-// const phone = new Sequelize(database, username, password, {
-//   host,
-//   dialect: 'postgres',
-//   port
-// });
-
-const phone = new Sequelize("postgres://default:Pi4nSqCAcx2W@ep-restless-frost-a4b9orug.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
-  dialectModule: require('pg')
+const phone = new Sequelize(database, username, password, {
+  host,
+  dialect: 'postgres',
+  port
 });
+
+// const phone = new Sequelize("postgres://default:Pi4nSqCAcx2W@ep-restless-frost-a4b9orug.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+//   dialectModule: require('pg')
+// });
 
 module.exports = phone;
